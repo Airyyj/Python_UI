c68270ff52f414c779fb4cc4454720e0826ea555
BlueSkyyj <chinayyj2012@sina.com>
2017-07-24 23:50:53 +0800

    Update __init__.py
    修改基本信息

--- UiTest/baseinfo/__init__.py
+++ UiTest/baseinfo/__init__.py
@@ -1,34 +1,27 @@
 #-*-coding:utf-8-*-
 #测试用例配置参数
 
-base_url = "http://www.yiyao.cc"
-undirect_username = "xinjiang1"
-undirect_password = "abc123"
-direct_username = "tstzj"
-direct_password = "abc123"
+base_url = "http://www."
+undirect_username = ""
+undirect_password = ""
+direct_username = ""
+direct_password = ""
 
-buyerName = 'xzuib'
+buyerName = ''
 buyerPassword = '111111'
 
-sellerName = 'xzuis'
+sellerName = ''
 sellerPassword = '111111'
 
 
 #发送邮件配置参数
-Smtp_Server = 'smtp.mxhichina.com'
-Smtp_Sender = 'dev@tenez.cn'
-Smtp_Sender_Password = 'Mall20151118'
-#Smtp_Receiver = ['yanglei@tenez.cn','yangyaojun@tenez.cn','zhailibao@tenez.cn','pengfei@tenez.cn']
-Smtp_Receiver = ['yangyaojun@tenez.cn']
-Smtp_Receiver_pre = ['zhailibao@tenez.cn']
+Smtp_Server = ''
+Smtp_Sender = ''
+Smtp_Sender_Password = ''
+#Smtp_Receiver = []
+Smtp_Receiver = []
+Smtp_Receiver_pre = []
 
-
-#测试邮箱配置
-#Smtp_Server = 'smtp.163.com'
-#Smtp_Sender = 'wmqyyj_test@163.com'
-#Smtp_Sender_Password = 'WMQyyj2018'
-#Smtp_Receiver = ['yangyaojun@tenez.cn']
-#Smtp_Receiver_pre = ['yangyaojun@tenez.cn']
 
 
 #测试用例及报告路径配置参数
